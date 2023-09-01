0ae4503079f4cee56d06fe09b8d42c60025f65df
Afeezope <afeezope1994@gmail.com>
2023-09-01 13:18:18 -0700

    Script to handle http Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that send a request to a given URL,
+- displays the response body(decoded in utf-8) and
+- Handles HTTP errors.
+"""
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
